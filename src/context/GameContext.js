947e740e039f5698293c9188737e3e205883b5d2
Sahda Samier <sahdasamier@gmail.com>
2023-03-02 18:07:13 +0200

    update

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -10,7 +10,7 @@ const GameState = (props) => {
     const [playMode, setPlayMode] = useState("user"); // user || cpu
     const [activeUser, setActiveUser] = useState("x"); // x || o
     const [squares, setSquares] = useState(new Array(9).fill(""));
-    const [xnext, setXnext] = useState(false);
+    const [xnext, setXnext] = useState("true");
     const [winner, setWinner] = useState(null);
     const [winnerLine, setWinnerLine] = useState(null);
     const [ties, setTies] = useState({x: 0, o: 0, no:0});
@@ -26,13 +26,14 @@ const GameState = (props) => {
            
            checkNoWinner ();
      // eslint-disable-next-line react-hooks/exhaustive-deps
-     } , [xnext ,winner,screen,ties]); //we use that not handleClick becacuse if i played with cup , and that three cases which changed , to check winner 
+     } , [xnext, winner, screen, ties]); //we use that not handleClick becacuse if i played with cup , and that three cases which changed , to check winner 
     
     
-    const changePlayMode = mode => {
-        setPlayMode(mode);
+     const handleStart = (player) => {
+        setPlayMode(player);
         setScreen("game");
-    }
+      };
+
     const handleSquareClick = (ix) => {
         if(squares[ix]|| winner) { //stop for changing ix after choose it 
             return ;
@@ -41,6 +42,7 @@ const GameState = (props) => {
         if (playMode === "cpu" && currentUser !== activeUser) {
             return;
         }
+
         let ns = [...squares];
         ns[ix] = !xnext ? "x" : "o"; // that mean i send (xi) as the value of click so i said here if the xplayer play now fill with X (notnext"xnext)if not fill wuth o
         setSquares(ns); // fill the suare with anew value
@@ -61,7 +63,7 @@ const GameState = (props) => {
 
     const handleNextRound = () => {
         setSquares(new Array(9).fill(''))
-        setXnext(winner === '0') // the winner will start , if winner=o that mean xnext was true
+        setXnext(winner === "o") // the winner will start , if winner=o that mean xnext was true
         setWinner(null)
         setWinnerLine(null)
         hideModal()
@@ -93,7 +95,7 @@ const GameState = (props) => {
           showModal();
           setModalMode("winner");
           
-     
+          
         
         }
       };
@@ -117,10 +119,10 @@ const GameState = (props) => {
             winnerLine,
             xnext,
             ties,
+            handleStart,
             handleSquareClick,
             setSquares,
             setScreen,
-            changePlayMode,
             setActiveUser,
             setPlayMode,
             setTies,
